33d2b1c6df03bdadf1b122f3a090a022bc911964
gksruf2848 <4455qqq@naver.com>
2023-10-12 18:03:59 +0900

    create commit

---
+++ src/test/java/com/emanguri/discordbottest/DiscordBotTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanguri.discordbottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBotTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
